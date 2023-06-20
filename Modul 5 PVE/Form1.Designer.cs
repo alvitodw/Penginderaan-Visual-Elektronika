cb463bcf1fb778fee3cd4c99256fb8d94352a45e
Alvito Dwinovan <90097108+alvitodw@users.noreply.github.com>
2023-06-20 14:39:32 +0700

    Add files via upload

---
+++ Modul 5 PVE/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Modul_5_PVE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxImgResult = new System.Windows.Forms.PictureBox();
+            this.pictureBoxImg2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxImg1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImgResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Proses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Proses GDI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxImgResult
+            // 
+            this.pictureBoxImgResult.Location = new System.Drawing.Point(586, 68);
+            this.pictureBoxImgResult.Name = "pictureBoxImgResult";
+            this.pictureBoxImgResult.Size = new System.Drawing.Size(164, 266);
+            this.pictureBoxImgResult.TabIndex = 2;
+            this.pictureBoxImgResult.TabStop = false;
+            // 
+            // pictureBoxImg2
+            // 
+            this.pictureBoxImg2.Image = global::Modul_5_PVE.Properties.Resources.pas_foto;
+            this.pictureBoxImg2.Location = new System.Drawing.Point(310, 68);
+            this.pictureBoxImg2.Name = "pictureBoxImg2";
+            this.pictureBoxImg2.Size = new System.Drawing.Size(185, 266);
+            this.pictureBoxImg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImg2.TabIndex = 1;
+            this.pictureBoxImg2.TabStop = false;
+            // 
+            // pictureBoxImg1
+            // 
+            this.pictureBoxImg1.Image = global::Modul_5_PVE.Properties.Resources.KTM_Unpad;
+            this.pictureBoxImg1.Location = new System.Drawing.Point(34, 68);
+            this.pictureBoxImg1.Name = "pictureBoxImg1";
+            this.pictureBoxImg1.Size = new System.Drawing.Size(178, 266);
+            this.pictureBoxImg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImg1.TabIndex = 0;
+            this.pictureBoxImg1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(232, 78);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Add";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(232, 101);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Substract";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(232, 124);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(74, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Difference";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(232, 147);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(60, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Multiply";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(232, 170);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(65, 17);
+            this.radioButton5.TabIndex = 9;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Average";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Image 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Image 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(649, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Image Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(532, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxImgResult);
+            this.Controls.Add(this.pictureBoxImg2);
+            this.Controls.Add(this.pictureBoxImg1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImgResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImg1;
+        private System.Windows.Forms.PictureBox pictureBoxImg2;
+        private System.Windows.Forms.PictureBox pictureBoxImgResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Modul 5 PVE/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Modul_5_PVE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp1 = (Bitmap)pictureBoxImg1.Image;
+            Bitmap bmp2 = (Bitmap)pictureBoxImg2.Image;
+            pictureBoxImgResult.Image = Subtraction(bmp2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmp1 = (Bitmap)pictureBoxImg1.Image;
+            Bitmap bmp2 = (Bitmap)pictureBoxImg2.Image;
+            pictureBoxImgResult.Image = SubtractionGDI(bmp2);
+        }
+
+        Bitmap bmp1;
+        public Bitmap Subtraction(Bitmap bmp2)
+        {
+         BitmapData bmpData1 = bmp1.LockBits(new Rectangle( 0 , 0 , bmp1.Width , bmp1.Height) , ImageLockMode.ReadWrite , PixelFormat.Format24bppRgb ) ;
+         BitmapData bmpData2 = bmp2.LockBits(new Rectangle( 0 , 0 , bmp2.Width , bmp2.Height) , ImageLockMode.ReadWrite , PixelFormat.Format24bppRgb ) ;
+        int width = bmpData1.Width;
+        int height = bmpData1.Height ;
+            Bitmap bmpresult = new Bitmap(width , height);
+        BitmapData bmpData3 = bmpresult.LockBits(new Rectangle( 0 , 0 ,bmpresult.Width , bmpresult.Height) , ImageLockMode.ReadWrite ,PixelFormat.Format24bppRgb ) ;
+         unsafe
+            {
+             int remain1 = bmpData1.Stride - bmpData1.Width * 3 ;
+             int remain2 = bmpData2.Stride - bmpData2.Width * 3 ;
+             int remain3 = bmpData3.Stride - bmpData3.Width * 3 ;
+             byte * ptr1 = (byte *) bmpData1.Scan0 ;
+             byte * ptr2 = (byte *) bmpData2.Scan0 ;
+            byte * ptr3 = (byte *) bmpData3.Scan0 ;
+                for (int i = 0 ; i<height; i++)
+                  {
+                    for (int j = 0 ; j<width *3 ; j++)
+                  {
+                    ptr3 [ 0 ] = (byte ) Math.Max((ptr1 [ 0 ] - ptr2 [ 0 ] ) , 0 ) ;
+                    ++ptr1 ;
+                    ++ptr2 ;
+                    ++ptr3 ;
+                    }
+                    ptr1 += remain1 ;
+                    ptr2 += remain2 ;
+                    ptr3 += remain3 ;
+                   }
+                }
+            bmp1.UnlockBits ( bmpData1 ) ;
+            bmp2.UnlockBits ( bmpData2 ) ;
+            bmpresult.UnlockBits( bmpData3 ) ;
+            return bmpresult ;
+    }
+          public Bitmap SubtractionGDI(Bitmap bmp2)
+          {
+            int width = bmp1.Width;
+            int height = bmp1.Height ;
+            int r, g, b;
+            Bitmap bmpresult = new Bitmap(width , height );
+            for (int i = 0 ; i<height ; i++)
+                {
+                for (int j = 0 ; j<width ; j++)
+            {
+                r=Math.Max(bmp1.GetPixel(i , j ).R-bmp2.GetPixel(i , j ).R, 0 ) ;
+                g=Math.Max(bmp1.GetPixel(i , j ).G-bmp2.GetPixel(i , j ).G, 0 ) ;
+                b=Math.Max(bmp1.GetPixel(i , j ).B-bmp2.GetPixel(i , j ).B, 0 ) ;
+                bmpresult.SetPixel(i, j, Color.FromArgb (r, g, b) );
+            }
+            }
+            return bmpresult ;
+            }
+
+    }
+}
